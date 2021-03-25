e8fdc1b0c0d735dd541517973dce5ff2282a9b0b
Alice Ribeiro <aliceoliveiraribeiro@gmail.com>
2021-03-25 13:09:19 -0300

    feat: fazendo a integração com a API, criando a listagem de candidatos e instalando o bootstrap e o bootstrap icons

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'new-hire';
+export class AppComponent implements OnInit {
+
+  public database$: Observable<any>;
+
+  constructor(private http: HttpClient) {
+
+  }
+
+  ngOnInit() {
+    this.http.get("https://randomuser.me/api/?nat=br&seed=5165165165&results=10")
+      .subscribe(d=> console.log(d));
+    this.database$ = this.http.get("https://randomuser.me/api/?nat=br&seed=5165165165&results=10");
+  }
+
 }
