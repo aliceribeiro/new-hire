8f6b2a78067fc5d649c9402dbed5fdeaf2e26766
Alice Ribeiro <aliceoliveiraribeiro@gmail.com>
2021-03-26 20:26:07 -0300

    feat: adicionando as tratativas e estilizando as informações da seção 'Atendidos'

--- src/app/checked/checked.component.ts
+++ src/app/checked/checked.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Candidate } from '../candidate-info/candidate';
+import { CandidateDataService } from '../candidate-info/candidate-data.service';
 @Component({
   selector: 'app-checked',
   templateUrl: './checked.component.html',
@@ -7,9 +9,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CheckedComponent implements OnInit {
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(
+    private candidateDataService: CandidateDataService
+  ) {
   }
 
+  public candidates: Candidate[] = [];
+
+  ngOnInit() { 
+    this.candidateDataService.getAllFromChecked().subscribe((result) => {
+      this.candidates = result
+    })
+  }
+
+  moverParaLixeira(uuid: string) {
+    this.candidateDataService.moveFromAllToTrash(uuid)
+  }
+  
 }
