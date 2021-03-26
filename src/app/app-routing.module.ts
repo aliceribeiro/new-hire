dc6deb196c163194bd9ecf0765225ece34fcff9b
Alice Ribeiro <aliceoliveiraribeiro@gmail.com>
2021-03-26 11:33:52 -0300

    feat: adicionando as rotas para home e page not found

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,16 +6,16 @@ import { HomeComponent } from './home/home.component';
 import { CheckedComponent } from './checked/checked.component';
 import { TrashComponent } from './trash/trash.component';
 import { CandidateInfoComponent } from './candidate-info/candidate-info.component';
-// import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 const routes: Routes = [
 
-  // {path: '', redirectTo: '/home', pathMatch: 'full'},
+  {path: '', redirectTo: '/home', pathMatch: 'full'},
   {path: 'home', component: HomeComponent},
   {path: 'checked', component: CheckedComponent},
   {path: 'trash', component: TrashComponent},
   {path: 'candidate-info', component: CandidateInfoComponent},
-  // {path: '**', component: PageNotFoundComponent}
+  {path: '**', component: PageNotFoundComponent}
 
 ]
 
