e0b1dbfdb50a6765f2d695f9ad7581ed6557325f
Alice Ribeiro <aliceoliveiraribeiro@gmail.com>
2021-04-01 17:30:58 -0300

    refactor: deletando todos os arquivos criados anteriormente

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,20 +2,8 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Routes, RouterModule } from '@angular/router';
 
-import { HomeComponent } from './home/home.component';
-import { CheckedComponent } from './checked/checked.component';
-import { TrashComponent } from './trash/trash.component';
-import { CandidateInfoComponent } from './candidate-info/candidate-info.component';
-import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 const routes: Routes = [
-
-  {path: '', redirectTo: '/home', pathMatch: 'full'},
-  {path: 'home', component: HomeComponent},
-  {path: 'checked', component: CheckedComponent},
-  {path: 'trash', component: TrashComponent},
-  {path: 'candidate-info/:uuid', component: CandidateInfoComponent},
-  {path: '**', component: PageNotFoundComponent}
 
 ]
 
