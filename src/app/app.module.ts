8d7bf3c318350473048a77d9d1641447805e57e5
Alice Ribeiro <aliceoliveiraribeiro@gmail.com>
2021-04-01 18:24:36 -0300

    feat: adicionando e importando o componente 'sidebar'

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
+    SidebarComponent,
   ],
   imports: [
     BrowserModule,
