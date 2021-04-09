0b662a415cc6d49ec74c897440aeffecbf5284a4
Alice Ribeiro <aliceoliveiraribeiro@gmail.com>
2021-04-09 15:39:38 -0300

    feat: importando os módulos de listagem de candidatos e HttpClient

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { TopbarComponent } from './topbar/topbar.component';
+import { candidatesListComponent } from './candidates/candidate-list/candidate-list.component';
 
 
 @NgModule({
@@ -12,10 +14,12 @@ import { TopbarComponent } from './topbar/topbar.component';
     AppComponent,
     SidebarComponent,
     TopbarComponent,
+    candidatesListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
